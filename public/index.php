af9d27c26e3e96b268be3b40d0d2c352893ba4f7
mrkabbaj <mr.kabbaj.hassan@gmail.com>
2017-09-18 03:42:18 +0100

    First commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+require '../vendor/autoload.php';
+
+$app = new \Framework\App();
+
+$demo = array();
+
+$response = $app->run(\GuzzleHttp\Psr7\ServerRequest::fromGlobals());
+
+\Http\Response\send($response);
